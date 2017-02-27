f46a9c0ab0998a525a4e705b726a6ebb602e378b
Jake Campa and Jason Brown <jake+jason@>
2017-02-27 14:53:16 -0800

    initialize base files create tag class save test passes

---
+++ src/Tag.php
@@ -0,0 +1,52 @@
+<?php
+    Class Tag
+    {
+        private $name;
+        private $id;
+
+        function __construct($name,$id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO tags (tag_name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_tags = $GLOBALS['DB']->query("SELECT * FROM tags;");
+            $tags = array();
+            foreach($returned_tags as $tag) {
+                $name = $tag['tag_name'];
+                $id = $tag['id'];
+                $new_tag = new Tag($name, $id);
+                array_push($tags, $new_tag);
+            }
+            return $tags;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM tags;");
+        }
+    }
+?>
---
+++ tests/TagTest.php
@@ -0,0 +1,55 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Tag.php";
+    // require_once "src/Post.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=blog_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CategoryTag extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Tag::deleteAll();
+        }
+        function test_save()
+        {
+            $name = "Thailand";
+            $new_tag = new Tag($name, null);
+
+            $new_tag->save();
+
+            $result = Tag::getAll();
+            $this->assertEquals($new_tag, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Thailand";
+            $new_tag = new Tag($name);
+            var_dump($new_tag);
+            $new_tag->save();
+            $result = Tag::getAll();
+
+            $this->assertEquals($new_tag, $result[0]);
+        }
+        function test_deleteAll()
+        {
+            $name = "Thailand";
+            $new_tag = new Tag($name);
+
+            $new_tag->save();
+            Tag::deleteAll();
+            $result = Tag::getAll();
+
+            $this->assertEquals([], $result);
+        }
+    }
+
+?>
